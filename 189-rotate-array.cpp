e6a81ce83407f6d3e8706cdd65a6c618696f0b0a
seokmo.yoo <seokmo.yoo@navercorp.com>
2024-05-14 23:58:43 +0900

    feat: added solution

--- 189-rotate-array.cpp
+++ 189-rotate-array.cpp
@@ -4,7 +4,8 @@ public:
         // rotate1(nums, k);
         // rotate2(nums, k);
         // rotate3(nums, k);
-        rotate4(nums, k);
+        // rotate4(nums, k);
+        rotate5(nums, k);
     }
 
     void rotate1(vector<int>& nums, int k) {
@@ -60,13 +61,27 @@ public:
         int size = nums.size();
         int buffer[size];
         int newK = k % size;
-        for (int i = 0; i < size; ++i) {
-            buffer[i] = nums[(size + i - newK) % size];
+        reverse(nums, 0, size - 1);
+        reverse(nums, 0, newK - 1);
+        reverse(nums, newK, size - 1);
+    }
+
+    void reverse(vector<int>& nums, int from, int to) {
+        int size = to - from + 1;
+        if (from >= to) {
+            return;
         }
-        
-        nums.clear();
-        for (int i = 0; i < size; ++i) {
-            nums.push_back(buffer[i]);
+        for (int i = 0; i < size / 2; ++i) {
+            iter_swap(nums.begin() + from + i, nums.begin() + to - i);
         }
     }
+
+    // [1,2,3,4,5,6,7]
+    // [7,2,3,4,5,6,1]
+    // [7,6,3,4,5,2,1]
+    // [7,6,5,4,3,2,1]
+
+    // [(5,6,7),(1,2,3,4)]
+    // [(6,7),(1,2,3,4,5)]
 };
+
